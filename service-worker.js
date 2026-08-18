7ec7377c8182bed02d3d478387383909e793ee0f
keys2autosales <keandre.scottbb@gmail.com>
2026-08-18 13:41:34 -0700

    Refresh PWA cache for inventory import fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='sales-command-v5';
+const CACHE='sales-command-v6';
 const ASSETS=['./','./index.html','./styles.css','./mockup-theme.css','./app.js','./marketplace-cloud.js','./marketplace-mockup.js','./inventory-cloud-import.js','./desktop-shell.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))).then(()=>self.skipWaiting()));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))).then(()=>self.clients.claim()));
