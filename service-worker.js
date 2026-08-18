52a172d6490963f882703b91d0d9ae19ce2f8c93
keys2autosales <keandre.scottbb@gmail.com>
2026-08-18 12:33:08 -0700

    Refresh PWA cache for Marketplace mockup UI

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='sales-command-v3';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./marketplace-cloud.js','./manifest.webmanifest'];
+const CACHE='sales-command-v4';
+const ASSETS=['./','./index.html','./styles.css','./mockup-theme.css','./app.js','./marketplace-cloud.js','./marketplace-mockup.js','./desktop-shell.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))).then(()=>self.skipWaiting()));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))).then(()=>self.clients.claim()));
 self.addEventListener('fetch',e=>{
